893125bddc8c726c3d642a0e62739fa162a33da5
Pavel J <pavelgj@gmail.com>
2013-08-12 22:27:40 -0700

    initial version

---
+++ lib/perf.dart
@@ -0,0 +1,27 @@
+library perf;
+
+/**
+ * A simple profiler api.
+ */
+abstract class Profiler {
+  /**
+   * Times execution of the [body].
+   */
+  Object time(String descr, Function body);
+
+  /**
+   * Starts a timer for a given [descr]. Allows nested timers.
+   */
+  void startTimer(String descr);
+
+  /**
+   * Stop a timer for a given [descr]. A timer must already be started,
+   * pthrrwise it will throw an error.
+   */
+  void stopTimer(String descr);
+
+  /**
+   * A simple zero-duration marker.
+   */
+  void markTime(String descr);
+}
