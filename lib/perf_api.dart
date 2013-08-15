7ee344c234cfa0f649f19e30d7c02f191305c19e
Pavel Jbanov <pavelgj@gmail.com>
2013-08-15 09:57:57 -0700

    minor api changes

--- lib/perf_api.dart
+++ lib/perf_api.dart
@@ -4,24 +4,41 @@ library perf_api;
  * A simple profiler api.
  */
 abstract class Profiler {
-  /**
-   * Times execution of the [body].
-   */
-  Object time(String descr, Function body);
 
   /**
-   * Starts a timer for a given [descr]. Allows nested timers.
+   * Times execution of the [body]. Body can either be a no argument function,
+   * or a Future. If function, it is executed synchronously and its return value
+   * is returned. If it's a Future, then timing is stopped when the future
+   * completes either successfully or with error. The "then" of the future with
+   * same result values is returned. See [startTimer] for more information
+   * about [extraData].
    */
-  void startTimer(String descr);
+  dynamic time(String id, body, [String extraData]);
+
+  /**
+   * Starts a timer for a given action [id]. If the timer is already started,
+   * this method will throw a [TimerError]. [extraData] is additional
+   * infromation about the timed action. Implementing profiler is free to
+   * ignore it.
+   */
+  void startTimer(String id, [String extraData]);
 
   /**
    * Stop a timer for a given [descr]. A timer must already be started,
-   * pthrrwise it will throw an error.
+   * otherwise it will throw a [TimerError].
    */
-  void stopTimer(String descr);
+  void stopTimer(String id);
 
   /**
    * A simple zero-duration marker.
    */
-  void markTime(String descr);
+  void markTime(String id);
+}
+
+class TimerError extends Error {
+  String message;
+
+  TimerError(String message);
+
+  toString() => message;
 }
